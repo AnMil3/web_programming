1542cf813f16b3db90d65a1da2030a4fba8beaa7
Andrej <andrejmile04@gmail.com>
2025-12-18 08:37:10 +0100

    add lab 4

--- src/main/java/mk/ukim/finki/wp/lab/WpLab1Application.java
+++ src/main/java/mk/ukim/finki/wp/lab/WpLab1Application.java
@@ -2,6 +2,9 @@ package mk.ukim.finki.wp.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class WpLab1Application {
@@ -9,5 +12,11 @@ public class WpLab1Application {
     public static void main(String[] args) {
         SpringApplication.run(WpLab1Application.class, args);
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
 
 }
